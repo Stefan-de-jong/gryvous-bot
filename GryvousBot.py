41ca828261ff25673124bab15c964b24c19c3af6
Stefan <sjf.de.jong@gmail.com>
2021-09-27 23:16:53 +0200

    Update

--- GryvousBot.py
+++ GryvousBot.py
@@ -1,22 +1,17 @@
-import variables
 import praw
 import re
 import os
-import random
+import time
+from dotenv import load_dotenv
+load_dotenv()
 
-reddit = praw.Reddit('GryvousBot', client_id=variables._client_id, client_secret=variables._client_secret, username=variables._username, password=variables._password)
+reddit = praw.Reddit(    
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    username=os.getenv('BOT_USERNAME'),
+    password=os.getenv('BOT_PASSWORD'),
+    user_agent="GryvousBot:v0.3 (by /u/Kapt_Roodbaard)")
 subreddit = reddit.subreddit('pythonforengineers')
-
-# Define some quotes to reply with
-gryvous_quotes = \
-[
-"How does it feel to die?",
-"Your lightsabers will make a fine addition to my collection!",
-"I’m no errand boy. I am not in this war for Dooku’s politics. I am the leader of the most powerful droid army the galaxy has ever seen.",
-"Anakin Skywalker, I expected someone with your reputation to be a little… older.",
-"General Kenobi, you are a bold one.",
-"I will deal with this Jedi slime myself."
-]
 
 # Have we run this code before? If not, create an empty list
 if not os.path.isfile("comments_replied_to.txt"):
@@ -40,19 +35,16 @@ for submission in subreddit.hot(limit=20):
             # Do a case insensitive search
             if re.search("hello there", comment.body, re.IGNORECASE):
                 # Reply to the post
-                comment.reply("General Kenobi, you are a bold one.")
                 print("Bot replying to : ", comment.id)
+                comment.reply("General Kenobi, you are a bold one.")                
+
                 # Store the current id into our list
                 comments_replied_to.append(comment.id)
-
-            if re.search("!Gryvous", comment.body, re.IGNORECASE):
-                # Reply with a random quote
-                comment.reply(random.choice(gryvous_quotes))
-                print("Bot quoting to : ", comment.id)
-                # Store the current id into our list
-                comments_replied_to.append(comment.id)
-
+            
 # Write our updated list back to the file
 with open("comments_replied_to.txt", "w") as f:
     for comment_id in comments_replied_to:
         f.write(comment_id + "\n")
+
+print("Sleeping for 10 sec...")
+time.sleep(10)
--- variables.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-_client_id=os.getenv('CLIENT_ID')
-_client_secret=os.getenv('CLIENT_SECRET')
-_password=os.getenv('BOT_PASSWORD')
-_username=os.getenv('BOT_USERNAME')
