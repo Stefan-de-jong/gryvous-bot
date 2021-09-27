aa1626da5e37fdceca8099cb9b7a5e7cf02f53a5
Stefan <sjf.de.jong@gmail.com>
2021-09-28 00:16:43 +0200

    update

--- GryvousBot.py
+++ GryvousBot.py
@@ -1,7 +1,7 @@
 import praw
 import re
 import os
-import time
+#import logging
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,8 +10,15 @@ reddit = praw.Reddit(
     client_secret=os.getenv('CLIENT_SECRET'),
     username=os.getenv('BOT_USERNAME'),
     password=os.getenv('BOT_PASSWORD'),
-    user_agent="GryvousBot:v0.3 (by /u/Kapt_Roodbaard)")
+    user_agent="GryvousBot:v0.4 (by /u/Kapt_Roodbaard)")
 subreddit = reddit.subreddit('pythonforengineers')
+
+# handler = logging.StreamHandler()
+# handler.setLevel(logging.DEBUG)
+# for logger_name in ("praw", "prawcore"):
+#     logger = logging.getLogger(logger_name)
+#     logger.setLevel(logging.DEBUG)
+#     logger.addHandler(handler)
 
 # Have we run this code before? If not, create an empty list
 if not os.path.isfile("comments_replied_to.txt"):
@@ -25,26 +32,25 @@ else:
         comments_replied_to = comments_replied_to.split("\n")
         comments_replied_to = list(filter(None, comments_replied_to))
 
-# Get the latest 20 upvotes posts from our subreddit
-for submission in subreddit.hot(limit=20):
-    # For every comment in the post
-    for comment in submission.comments:
-        # If we haven't replied to this post before
-        if comment.id not in comments_replied_to:
+print("Starting")
 
-            # Do a case insensitive search
-            if re.search("hello there", comment.body, re.IGNORECASE):
-                # Reply to the post
-                print("Bot replying to : ", comment.id)
-                comment.reply("General Kenobi, you are a bold one.")                
+# For every comment in the post
+for comment in subreddit.comments(limit=250):
+    # If we haven't replied to this post before
+    if comment.id not in comments_replied_to:
 
-                # Store the current id into our list
-                comments_replied_to.append(comment.id)
+        # Do a case insensitive search
+        if re.search("hello there", comment.body, re.IGNORECASE):
+            # Reply to the post
+            print("Bot replying to : ", comment.id)
+            comment.reply("General Kenobi, you are a bold one.")                
+
+            # Store the current id into our list
+            comments_replied_to.append(comment.id)
             
 # Write our updated list back to the file
 with open("comments_replied_to.txt", "w") as f:
     for comment_id in comments_replied_to:
         f.write(comment_id + "\n")
 
-print("Sleeping for 10 sec...")
-time.sleep(10)
+print("Done")
